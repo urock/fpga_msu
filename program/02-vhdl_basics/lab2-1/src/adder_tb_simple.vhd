d12a2ff0334663f87d52ff44dbe693205c3a9d8d
urock <yarumyantsen@gmail.com>
2016-07-05 12:08:09 +0300

    some fixes in including code from files

--- program/02-vhdl_basics/lab2-1/src/adder_tb_simple.vhd
+++ program/02-vhdl_basics/lab2-1/src/adder_tb_simple.vhd
@@ -1,58 +1,57 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
-ENTITY adder_tb IS
-END adder_tb;
+entity adder_tb IS
+end adder_tb;
 
-ARCHITECTURE behavior OF adder_tb IS
+architecture behavior of adder_tb IS
 
-    COMPONENT adder_top
-    PORT(
-         switch_in : IN  std_logic_vector(7 downto 0);
-         leds_out : OUT  std_logic_vector(7 downto 0)
-        );
-    END COMPONENT;
+   component adder_top
+   port(
+      switch_in : in  std_logic_vector(7 downto 0);
+      leds_out : out  std_logic_vector(7 downto 0)
+   );
+   end component;
 
    --Inputs
    signal switch_in : std_logic_vector(7 downto 0) := (others => '0');
 
- 	--Outputs
+   --Outputs
    signal leds_out : std_logic_vector(7 downto 0);
 
-	signal A, B	: std_logic_vector(3 downto 0);
+   signal A, B  : std_logic_vector(3 downto 0);
 
+begin
 
-BEGIN
+   -- Instantiate the Design Under Test (DUT)
+   dut: adder_top port map (
+      switch_in => switch_in,
+      leds_out => leds_out
+   );
 
-	-- Instantiate the Unit Under Test (UUT)
-   uut: adder_top PORT MAP (
-          switch_in => switch_in,
-          leds_out => leds_out
-        );
-
-	switch_in	<= B & A;
+   switch_in <= B & A;
 
    -- Stimulus process
    stim_proc: process
    begin
-		A <= "0000";
-		B <= "0000";
+      A <= "0000";
+      B <= "0000";
       wait for 100 ns;
 
-		A <= "0001";
-		B <= "0100";
+      A <= "0001";
+      B <= "0100";
       wait for 100 ns;
 
-		A <= "0011";
-		B <= "0101";
+      A <= "0011";
+      B <= "0101";
       wait for 100 ns;
 
-		A <= "0001";
-		B <= "0100";
+      A <= "0001";
+      B <= "0100";
       wait for 100 ns;
 
-		A <= "0001";
-		B <= "0111";
+      A <= "0001";
+      B <= "0111";
       wait;
    end process;
-END;
+end;
